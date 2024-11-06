406f747da01765d55b9483443593e6fc044d8bc9
XJJamazing <43208109+XJJamazing@users.noreply.github.com>
2024-11-06 21:02:58 +0800

    Add files via upload

---
+++ lab4/flik/test.java
@@ -0,0 +1,17 @@
+package flik;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by hug.
+ */
+public class test {
+    // YOUR TESTS HERE
+    @Test
+    public void testTrue(){
+        assertTrue(Flik.isSameNumber(129, 129));
+    }
+}
